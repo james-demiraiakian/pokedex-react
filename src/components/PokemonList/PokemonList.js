043725c4086c64e7e00f030a346ec28fa1085ebf
James Demiraiakian <james.armen@gmail.com>
2021-12-10 10:37:26 -0800

    removed unused import statement

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -5,7 +5,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { minWidth } from '@mui/system';
 
 export default function PokemonList({ pokemon, currentPage, setCurrentPage, setLoading }) {
   const nextPage = () => {
