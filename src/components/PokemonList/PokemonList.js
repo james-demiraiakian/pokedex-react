4b8dde7606aec936d3e850331732c2f0f2985d52
James Demiraiakian <james.armen@gmail.com>
2021-12-10 10:27:20 -0800

    finished adding material UI cards, fine tuning positions and other UI elements

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './PokemonList.css';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { minWidth } from '@mui/system';
 
 export default function PokemonList({ pokemon, currentPage, setCurrentPage, setLoading }) {
   const nextPage = () => {
@@ -15,20 +21,44 @@ export default function PokemonList({ pokemon, currentPage, setCurrentPage, setL
   };
 
   return (
-    <div className="pokemon-list">
-      {pokemon.map((poke) => (
-        <div key={poke.id} className="pokemon-card">
-          <p>{poke.pokemon}</p>
-          <img src={poke.url_image} height={100} />
-          <p>ID: {poke.id}</p>
-          <p>Type 1: {poke.type_1}</p>
-          {poke.type_2 !== 'NA' && <p>Type 2: {poke.type_2}</p>}
-          {poke.generation_id !== 'NA' && <p>Generation: {poke.generation_id}</p>}
-        </div>
-      ))}
-      <button onClick={previousPage}>Previous Page</button>
-      <div>Page: {currentPage}</div>
-      <button onClick={nextPage}>Next Page</button>
-    </div>
+    <>
+      <div className="pokemon-list">
+        {pokemon.map((poke) => (
+          <Card
+            sx={{ maxWidth: 320, minWidth: 300, margin: 2 }}
+            key={poke.id}
+            className="pokemon-card"
+          >
+            <CardMedia component="img" height="250" image={poke.url_image} alt={poke.pokemon} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {poke.pokemon}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                ID: {poke.id}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Type 1: {poke.type_1}
+              </Typography>
+              {poke.type_2 !== 'NA' && (
+                <Typography variant="body2" color="text.secondary">
+                  Type 2: {poke.type_2}
+                </Typography>
+              )}
+              {poke.generation_id !== 'NA' && (
+                <Typography variant="body2" color="text.secondary">
+                  Generation: {poke.generation_id}
+                </Typography>
+              )}
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <Button onClick={previousPage}>Previous Page</Button>
+      <Typography variant="body2" color="text.secondary">
+        Page: {currentPage}
+      </Typography>
+      <Button onClick={nextPage}>Next Page</Button>
+    </>
   );
 }
